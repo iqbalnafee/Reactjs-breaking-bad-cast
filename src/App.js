c9f81bf4f081e5a161b5fcf5620621ad6c16e8f7
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 10:49:55 +0600

    useEffect and http get request done with axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
 import Header from './components/ui/Header';
-
+import axios from 'axios';
 function App() {
 
   const [items,setItems] = useState([]);
   const [isLoading,setLoading] = useState(true);
-  
+
+  //to make the request we will use useEffect when the component is mounted
+
+  //useEffect use arrow functions 
+
+  useEffect( () =>{
+
+    //since we will use axios and axios return a promise we can do async with useEffect. Hence we will use an arrow function inside useEffect
+    const fetchItems = async () => {
+      const result = await axios.get(`https://www.breakingbadapi.com/api/characters`);
+      console.log(result.data);
+    }
+
+    fetchItems();
+
+  },[] ); //useEffect 2nd parameter is dependencies
 
   return (
     <div className="container">
