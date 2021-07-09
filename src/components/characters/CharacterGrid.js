4989a278097a2b7d1169aea618f81c7561783c70
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 11:12:19 +0600

    data is viewd in characterGrid

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const CharacterGrid = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const CharacterGrid = (props) => {
+
+    const {loading,items} = props;
+    return loading ? (<h1>Loading.....</h1>) :(<section className="cards">
+
+        {
+
+            items.map(
+
+                (item) => (<h1 key={item.char_id} >{item.name}</h1>)
+
+            )
+
+        }
+
+    </section>);
 }
 
 export default CharacterGrid
