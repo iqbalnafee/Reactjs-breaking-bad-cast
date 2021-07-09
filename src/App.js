82f7d88fc9aef4d434674e5f2afa65da9aedd34b
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 10:26:03 +0600

    Hedaer component and App.css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/ui/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/ui/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+
+            <header className="center">
+
+                <img src={logo} alt='' />
+
+            </header>
+            
+        </div>
+    )
+}
+
+export default Header
