d7423d10a3d4f8f877f775eb781f0e9e092a3b2c
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 11:00:03 +0600

    characterGrid added and props pass from app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 import axios from 'axios';
 function App() {
 
@@ -29,6 +30,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid loading={isLoading} items={items} />
     </div>
   );
 }
