acaba66793fa82d6c75d4a99ca730a249168f708
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 11:57:24 +0600

    Search component with form created

---
+++ src/components/ui/Search.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+const Search = () => {
+    return (
+        <section>
+
+            <form>
+
+                <input type="text" className="form-control" placeholder="Search Characters" autoFocus />
+
+            </form>
+
+        </section>
+    )
+}
+
+export default Search
