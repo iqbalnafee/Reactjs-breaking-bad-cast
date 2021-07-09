59d383b27e87c066db58979d640cf4f83f3f43b4
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 11:27:19 +0600

    cardItem styling done with front images

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -5,9 +5,37 @@ const CharacterItem = (props) => {
     const {item} = props;
 
     return (
-        <div>
+        <div className="card">
 
-            <h1>{item.name}</h1>
+            <div className="card-inner">
+
+                <div className="card-front">
+                    <img src={item.img} alt="" />
+                </div>
+
+                <div className="card-back">
+
+                    <h1>{item.name}</h1>
+
+                    <ul>
+                        <li><strong>Actor Name: </strong>{item.portrayed}</li>
+                    </ul>
+                    <ul>
+                        <li><strong>Nickname: </strong>{item.nickname}</li>
+                    </ul>
+                    <ul>
+                        <li><strong>Birthday: </strong>{item.birthday}</li>
+                    </ul>
+                    <ul>
+                        <li><strong>Status: </strong>{item.status}</li>
+                    </ul>
+
+                </div>
+
+
+            </div>
+
+            
             
         </div>
     )
